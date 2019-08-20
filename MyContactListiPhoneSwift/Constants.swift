00f3bdcd0521b7262b56c7b087e3235cc43c97fb
E. Muse <44449134+E-Muse@users.noreply.github.com>
2019-08-20 13:28:41 -0400

    Add files via upload

---
+++ MyContactListiPhoneSwift/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  MyContactListiPhoneSwift
+//
+//  Created by Emmanuel Muse on 4/10/19.
+//  Copyright © 2019 Emmanuel Muse. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let kSortField = "sortField"
+    static let kSortDirectionAscending = "sortDirectionAscending"
+    
+}
---
+++ MyContactListiPhoneSwift/ContactsViewController.swift
@@ -0,0 +1,123 @@
+//
+//  ContactsViewController.swift
+//  MyContactListiPhoneSwift
+//
+//  Created by Emmanuel Muse on 4/3/19.
+//  Copyright © 2019 Emmanuel Muse. All rights reserved.
+//
+
+
+
+import UIKit
+
+class ContactsViewController: UIViewController {
+    
+    var currentContact: Contact?
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    @IBOutlet weak var sgmtEditMode: UISegmentedControl!
+    @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var txtAddress: UITextField!
+    @IBOutlet weak var txtCity: UITextField!
+    @IBOutlet weak var txtState: UITextField!
+    @IBOutlet weak var txtZip: UITextField!
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtCell: UITextField!
+    @IBOutlet weak var txtPhone: UITextField!
+    @IBOutlet weak var lblBirthdate: UILabel!
+    @IBOutlet weak var btnChange: UIButton!
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+      
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func changeEditMode(_ sender: Any) {
+        let textFields: [UITextField] = [txtName, txtAddress, txtCity,
+                                         txtState, txtZip, txtPhone, txtCell, txtEmail]
+        if sgmtEditMode.selectedSegmentIndex == 0 {
+            for textField in textFields {
+                textField.isEnabled = false
+                textField.borderStyle = UITextField.BorderStyle.none
+            }
+            btnChange.isHidden = true
+        }
+        else if sgmtEditMode.selectedSegmentIndex == 1{
+            for textField in textFields {
+                textField.isEnabled = true
+                textField.borderStyle = UITextField.BorderStyle.roundedRect
+            }
+            btnChange.isHidden = false
+        }
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.registerKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.unregisterKeyboardNotifications()
+    }
+    
+    
+    
+    func registerKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self,
+            selector:#selector(ContactsViewController.keyboardDidShow(notification:)), name:
+            UIResponder.keyboardDidShowNotification, object: nil)
+        
+            NotificationCenter.default.addObserver(self, selector:
+            #selector(ContactsViewController.keyboardWillHide(notification:)), name:
+            UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func unregisterKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    @objc func keyboardDidShow(notification: NSNotification) {
+        let userInfo: NSDictionary = notification.userInfo! as NSDictionary
+        let keyboardInfo = userInfo[UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue
+        let keyboardSize = keyboardInfo.cgRectValue.size
+        
+        // Get the existing contentInset for the scrollView and set the bottom property to be the height of the keyboard
+        var contentInset = self.scrollView.contentInset
+        contentInset.bottom = keyboardSize.height
+        
+        self.scrollView.contentInset = contentInset
+        self.scrollView.scrollIndicatorInsets = contentInset
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        var contentInset = self.scrollView.contentInset
+        contentInset.bottom = 0
+        
+        self.scrollView.contentInset = contentInset
+        self.scrollView.scrollIndicatorInsets = UIEdgeInsets.zero
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
